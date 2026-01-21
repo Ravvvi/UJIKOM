d3632d1f09116175ffaf4763a5e5a65f103264e2
Ravvvi <dwijanuarwahyuravi@gmail.com>
2026-01-21 09:45:56 +0700

    fix: create new admin

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,24 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\OrderController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProductController::class, 'index']);
-Route::get('/create', [ProductController::class, 'create']);
-Route::post('/store', [ProductController::class, 'store']);
-Route::get('/delete/{id}', [ProductController::class, 'destroy']);
-Route::get('/edit/{id}', [ProductController::class, 'edit']);
-Route::post('/update/{id}', [ProductController::class, 'update']);
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
-Route::get('/delete/{id}', [ProductController::class, 'destroy']);
+
+Route::get('/orders', [OrderController::class, 'index']);
 Route::get('/checkout/{id}', [OrderController::class, 'checkout']);
 Route::post('/store-order', [OrderController::class, 'storeOrder']);
-Route::get('/orders', [OrderController::class, 'index']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/create', [ProductController::class, 'create']);
+    Route::post('/store', [ProductController::class, 'store']);
+    Route::get('/edit/{id}', [ProductController::class, 'edit']);
+    Route::post('/update/{id}', [ProductController::class, 'update']);
+    Route::get('/delete/{id}', [ProductController::class, 'destroy']);
+});

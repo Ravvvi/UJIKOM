1f85b9b143b7378b2307f5ace5f25629140261d3
Ravvvi <dwijanuarwahyuravi@gmail.com>
2026-04-06 10:58:04 +0700

    mengubah kategori di bagian edit

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -27,15 +27,15 @@
                         <div class="mb-3">
                             <label class="form-label fw-bold">Kategori</label>
                             <select name="category" class="form-select" required>
-                                <option value="">-- Pilih Kategori Sparepart --</option>
+                                 <option value="">-- Pilih Kategori Sparepart --</option>
                                 <option value="GPU" {{ old('category') == 'GPU' ? 'selected' : '' }}>GPU (Graphics Card)</option>
                                 <option value="CPU" {{ old('category') == 'CPU' ? 'selected' : '' }}>CPU (Processor)</option>
                                 <option value="RAM" {{ old('category') == 'RAM' ? 'selected' : '' }}>RAM (Memory)</option>
-                                <option value="Storage" {{ old('category') == 'Storage' ? 'selected' : '' }}>Storage (SSD/HDD)</option>
+                                <option value="SSD" {{ old('category') == 'SSD' ? 'selected' : '' }}>SSD</option>
+                                <option value="HDD" {{ old('category') == 'HDD' ? 'selected' : '' }}>HDD</option>
                                 <option value="Motherboard" {{ old('category') == 'Motherboard' ? 'selected' : '' }}>Motherboard</option>
                                 <option value="PSU" {{ old('category') == 'PSU' ? 'selected' : '' }}>PSU (Power Supply)</option>
                                 <option value="CASE" {{ old('category') == 'CasE' ? 'selected' : '' }}>CASE</option>
-                            </select>
                         </div>
 
                         <div class="row">
